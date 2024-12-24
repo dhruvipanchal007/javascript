8cbff1dd4dd6be928a356a4189d272b13ea0ece0
Dhruvi Panchal <dhruvipanchal007@gmail.com>
2024-12-24 10:50:16 +0530

    initial commit

---
+++ loops.js
@@ -0,0 +1,35 @@
+// Loops Programme
+
+// print table of given number
+// let number = 20;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} * ${i} = ${number * i}`)
+// }
+
+// Question 2
+// print table number give range number by user
+// let startnumber = 3;
+// let endnumber = 7;
+// let number = 4;
+
+// for (let i = startnumber; i <= endnumber; i++) {
+//     console.log(`${number}* ${i} = ${number * i} `)
+// }
+
+// question 3
+//find sum of n numbers
+// let number = 10;
+// let sum = 0;
+// for (let i = 1; i <= number; i++) {
+// 	sum += i;
+// }
+// console.log(sum);
+
+// question 4
+// print factorial number n
+// let number = 3;
+// let fact = 1;
+// for (let i = 1; i <= number; i++) {
+// 	fact *= i;
+// }
+// console.log(fact);
