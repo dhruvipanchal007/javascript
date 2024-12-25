22b4f6a9bf743e1f51598bfee7e1978437f078ce
Dhruvi Panchal <dhruvipanchal007@gmail.com>
2024-12-25 17:37:34 +0530

    add question

--- loops.js
+++ loops.js
@@ -33,3 +33,77 @@
 // 	fact *= i;
 // }
 // console.log(fact);
+
+// question 5
+// let number = [10, 5, 20, 100, 18];
+// let maximumnumber = '';
+// for (let i = 1; i <= number.length; i++) {
+// 	if (number[i] > maximumnumber) {
+// 		maximumnumber = number[i];
+// 	}
+// }
+// console.log(maximumnumber);
+
+// question 6
+// sum of digit entered by user
+// let sum = 0;
+// let number = 5;
+// console.log(number.length);
+// for (let i = 1; i <= number; i++) {
+// 	sum += number;
+// }
+// console.log(sum);
+
+// question 7
+// print fibonacciseries
+// let a = 0;
+// let b = 1;
+// console.log(a);
+// console.log(b);
+// for (let i = 1; i < 10; i++) {
+// 	let c = a + b;
+// 	console.log(c);
+// 	a = b;
+// 	b = c;
+// }
+
+// question 8
+// peint 100 odd number
+// for (let i = 1; i <= 100; i++) {
+// 	if (i % 2 !== 0) {
+// 		console.log(i);
+// 	}
+// }
+// even number
+// for (let i = 1; i <= 100; i++) {
+// 	if (i % 2 == 0) {
+// 		console.log(i);
+// 	}
+// }
+
+// question 9
+// number is palindrome or not
+// let number = 121;
+// let numberstring = number.toString();
+// let reverse = numberstring.split('').reverse('').join('');
+// console.log(reverse);
+// if (reverse === numberstring) {
+// 	console.log('number is palindrome');
+// } else {
+// 	console.log('number is not palindrome');
+// }
+
+// question 10
+// armstrong number`
+// let number = 153;
+// let numberstring = number.toString().split('');
+
+// question 11
+for (let i = 1; i <= 4; i++) {
+	let a = ' ';
+  for (let k=1;k <= )
+	for (let j = 1; j <= i; j++) {
+		a += '*';
+	}
+	console.log(a);
+}
